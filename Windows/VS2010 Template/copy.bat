2352fb99d30da57ba7865c70a18293d556897c5c
Peter Jesinger <jesinger@multichannelsystems.com>
2016-12-01 16:22:54 +0100

    updated template

--- Windows/VS2010 Template/copy.bat
+++ Windows/VS2010 Template/copy.bat
@@ -1,2 +1,3 @@
 copy *.zip "%USERPROFILE%\Documents\Visual Studio 2010\Templates\ProjectTemplates\"
 copy *.zip "%USERPROFILE%\Documents\Visual Studio 2013\Templates\ProjectTemplates\Visual C#\"
+copy *.zip "%USERPROFILE%\Documents\Visual Studio 2015\Templates\ProjectTemplates\Visual C#\"
