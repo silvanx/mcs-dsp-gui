12130c9b17afb218affc87712ea7aa9b96aae337
Kuba Orłowski <j.orlowski@student.uw.edu.pl>
2021-07-13 09:59:22 +0100

    Reverted button caption

--- Windows/DSP Dacq Example/Dacq.Designer.cs
+++ Windows/DSP Dacq Example/Dacq.Designer.cs
@@ -28,10 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.startDacq = new System.Windows.Forms.Button();
             this.dspData = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.series0Channel = new System.Windows.Forms.ComboBox();
@@ -77,22 +77,22 @@
             // 
             // dspData
             // 
-            chartArea1.Name = "ChartArea1";
-            this.dspData.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.dspData.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.dspData.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.dspData.Legends.Add(legend2);
             this.dspData.Location = new System.Drawing.Point(12, 165);
             this.dspData.Name = "dspData";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
-            series1.Legend = "Legend1";
-            series1.Name = "Chart 1";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
-            series2.Legend = "Legend1";
-            series2.Name = "Chart 2";
-            this.dspData.Series.Add(series1);
-            this.dspData.Series.Add(series2);
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series3.Legend = "Legend1";
+            series3.Name = "Chart 1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series4.Legend = "Legend1";
+            series4.Name = "Chart 2";
+            this.dspData.Series.Add(series3);
+            this.dspData.Series.Add(series4);
             this.dspData.Size = new System.Drawing.Size(952, 376);
             this.dspData.TabIndex = 6;
             this.dspData.Text = "DSP Data";
@@ -222,7 +222,7 @@
             this.UploadDSPBinary.Name = "UploadDSPBinary";
             this.UploadDSPBinary.Size = new System.Drawing.Size(184, 40);
             this.UploadDSPBinary.TabIndex = 34;
-            this.UploadDSPBinary.Text = "Threshold ON/OFF";
+            this.UploadDSPBinary.Text = "Upload DSP Binary";
             this.UploadDSPBinary.UseVisualStyleBackColor = true;
             this.UploadDSPBinary.Click += new System.EventHandler(this.UploadDSPBinary_Click);
             // 
