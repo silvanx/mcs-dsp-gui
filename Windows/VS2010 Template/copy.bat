6433732b8a5e1cd6a93444eab43bb205e5552d58
Kuba Orłowski <kuba.orlowski@protonmail.com>
2025-08-26 15:54:15 +0100

    Remove unnecessary template files

--- Windows/VS2010 Template/copy.bat
+++
@@ -1,3 +0,0 @@
-copy *.zip "%USERPROFILE%\Documents\Visual Studio 2010\Templates\ProjectTemplates\"
-copy *.zip "%USERPROFILE%\Documents\Visual Studio 2013\Templates\ProjectTemplates\Visual C#\"
-copy *.zip "%USERPROFILE%\Documents\Visual Studio 2015\Templates\ProjectTemplates\Visual C#\"
