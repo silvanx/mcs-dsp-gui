5b0daa9a88ba2f33068b1c0bb8762ba707857d48
Kuba Orłowski <kuba.orlowski@protonmail.com>
2025-10-22 15:26:15 +0100

    Rename Solution

--- Windows/DSP Dacq Example/Properties/AssemblyInfo.cs
+++ Windows/DSP Dacq Example/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MCS_USB_Windows_Forms_Application1")]
+[assembly: AssemblyTitle("MCS_Closed_Loop_Feedback_GUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("MCS_USB_Windows_Forms_Application1")]
+[assembly: AssemblyProduct("MCS_Closed_Loop_Feedback_GUI")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
